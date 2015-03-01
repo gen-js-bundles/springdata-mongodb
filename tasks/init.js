ae99fed8c5a85eedddbf5f78c60e391c8291392e
Ludovic Chaboud <l.chaboud@gmail.com>
2015-03-01 09:34:05 +0100

    Create init.js

---
+++ tasks/init.js
@@ -0,0 +1,14 @@
+var
+  inquirer = require("inquirer"),
+  fs = require('fs'),
+  path = require('path'),
+  gfile = require('gfilesync'),
+  yaml = require('js-yaml');
+
+module.exports = {
+  askFor: function(callback) {
+
+    console.log('Hello');
+    
+  }
+};
